1a8613b71f06f97ae4fa0c3542bfd47aac7ce4ac
sumangourab <sumangourab77@gmail.com>
2022-11-10 23:26:51 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <>
+      <p className="data-ns-test">project-name</p>
+      <p className="data-ns-test">project-description</p>
+      </>
+    </div>
   )
 }
 
