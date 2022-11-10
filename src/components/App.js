d4429f9ce34ee92bce607f5ea85c20da904f4a8c
sumangourab <sumangourab77@gmail.com>
2022-11-10 23:33:49 +0530

    r

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <div id="main">
       <>
-      <p data-ns-test>project-name</p>
-      <p data-ns-test>project-description</p>
+      <p data-ns-test = 'project-name'>b</p>
+      <p data-ns-test = 'project-description'>a</p>
       </>
     </div>
   )
